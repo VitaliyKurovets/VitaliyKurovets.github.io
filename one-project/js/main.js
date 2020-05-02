0ad42afea69d93d023b961bf9d9a305a0bb85998
VitaliyKurovets <vitaliy.kurovets@gmail.com>
2020-05-02 13:06:24 +0300

    Добавил ещё одну секцию с адаптацией.

--- one-project/js/main.js
+++ one-project/js/main.js
@@ -5,3 +5,13 @@ document.getElementById("burger-menu").onclick = function() {open()};
 function open() {
     document.getElementById("adaptive-menu").classList.toggle("menu");
 }
+
+// Функция для смены цвета фона при клике на кнопки
+
+function changeBg() {
+    document.querySelector(".lamp").style.background = "#FF9500";
+};
+
+function changeBg2() {
+    document.querySelector(".lamp").style.background = "#c97940";
+};
